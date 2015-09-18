186a0afe3239b01caa8bf8219d0a9ae35a6ee96c
Lisa <Lisa@IBM-X61-Lisa>
2015-09-18 20:28:25 +0200

    Add a new debug function -msg- without VAARGS

--- debug.h
+++ debug.h
@@ -28,6 +28,7 @@
 
 #define log_info(M, ...) fprintf(g_FILEOutputLogStream, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
+#define log_msg(M) fprintf(g_FILEOutputLogStream, "[MSG] (%s:%d) " M "\n", __FILE__, __LINE__)
 
 
 /** How to use :
